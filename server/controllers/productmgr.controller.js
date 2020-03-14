218eb67f40155ab25963e06edbc7635888f3cc06
frances <frncsbthn@gmail.com>
2020-03-13 20:19:14 -0700

    added update/delete functionality

--- server/controllers/productmgr.controller.js
+++ server/controllers/productmgr.controller.js
@@ -32,3 +32,15 @@ module.exports.getProduct = (req, res) => {
     .then(product => res.json(product))
     .catch(err => res.json(err));
 };
+
+module.exports.updateProduct = (req, res) => {
+  Product.findByIdAndUpdate({ _id: req.params.id }, req.body, { new: true })
+    .then(updatedProduct => res.json(updatedProduct))
+    .catch(err => res.json(err));
+};
+
+module.exports.deleteProduct = (req, res) => {
+  Product.findByIdAndDelete(req.params.id)
+    .then(() => res.json({ status: "success" }))
+    .catch(err => res.json(err));
+};
