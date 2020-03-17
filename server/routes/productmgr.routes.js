bf28151e7f7798b6caa498af7e6527af1ff64f96
frances <frncsbthn@gmail.com>
2020-03-16 21:38:36 -0700

    refactored code / practice with reusable code

--- server/routes/productmgr.routes.js
+++ server/routes/productmgr.routes.js
@@ -5,6 +5,6 @@ module.exports = function(app) {
   app.post("/api/products", ProductController.createProduct);
   app.get("/api/products", ProductController.allProducts);
   app.get("/api/products/:id", ProductController.getProduct);
-  app.put("/api/products/:id", ProductController.updateProduct);
+  app.put("/api/products/:id/edit", ProductController.updateProduct);
   app.delete("/api/products/:id", ProductController.deleteProduct);
 };
